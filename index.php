5405f925ad8a150f82790a6854203a4522be6492
rz0012 <rz0012@mix.wvu.edu>
2021-02-03 21:39:16 -0500

    initial

---
+++ index.php
@@ -0,0 +1,60 @@
+<body>
+    <link rel="stylesheet" href="css/about.css"> 
+
+    <div class="about">
+        <h1>About Them</h1>
+        <p>People Who Have The Imagination of The World of Tomorrow</p>
+
+    </div>
+    <div class="row">
+         <div class="column">
+             <div class="card">
+                <img src="images/elon-musk-1.webp">
+                <div class="container1">
+                    <h2>CEO</h2>
+                    <p>Elon Musk</p>
+                    <p>Email: <a href= "elon.musk@tesla.com">elon.musk@tesla.com</a></p>
+
+                </div> 
+             </div>
+         </div>
+         <div class="column">
+            <div class="card">
+               <img src="images/max-tegmark-1.jpg">
+               <div class="container2">
+                   <h2>Professor</h2>
+                   <p>Max Tegmark</p>
+                   <p>Email: <a href= "max.tegmark@mit.edu">max.tegmark@mit.edu</a></p>
+
+               </div> 
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+               <img src="images/sam-altman-1.webp">
+               <div class="container3">
+                   <h2>CEO</h2>
+                   <p>Sam Altman</p>
+                   <p>Email: <a href= "sam.altman@openai.com">sam.altman@openai.com</a></p>
+
+               </div> 
+            </div>
+        </div>
+        <div class="column">
+
+            <div class="card">
+               <img src="images/tomaso-poggio-1.png">
+               <div class="container4">
+                   <h2>Professor</h2>
+                   <p>Tomaso Poggio</p>
+                   <p>Email: <a href= "tomaso.poggio@mit.edu">tomaso.poggio@mit.edu</a></p>
+
+               </div> 
+            </div>
+        </div>
+        
+
+         
+    
+    </div>
+</body>
