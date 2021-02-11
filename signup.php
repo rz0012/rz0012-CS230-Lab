8f8b06a89aa2635d83f2b629a00120e68dfcf44e
rz0012 <rz0012@mix.wvu.edu>
2021-02-10 19:01:14 -0500

    part 1 to 4 completed

---
+++ signup.php
@@ -0,0 +1,51 @@
+<?php
+require "includes/header.php"
+?>
+
+<main>
+    <link rel="stylesheet" href="../css/signup.css">
+    <div class="bg-cover">
+
+        <div class="h-100 container center-me">
+            <div class="my-auto">
+                <div class="signup-form">
+
+                    <form action="includes/signup-helper.php" method="post">
+
+                        <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+
+                        <p class="hint-text"> Create your account! </p>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" name="fname" placeholder="First Name" required autofocus>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="lname" placeholder="Last Name" required autofocus>
+                                </div>
+                            </div>
+                        </div>
+
+                        <input type="text" class="form-control" name="uname" placeholder="User Name" required autofocus>
+
+                        <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required
+                            autofocus>
+
+                        <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
+                        
+                        <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password" required>
+
+                        <button class="btn btn-lg btn-outline-danger btn-block" name="signup-submit" type="submit">Sign in</button>
+                        <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+
+                    </form>
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+</main>
